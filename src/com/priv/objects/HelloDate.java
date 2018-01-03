9d75902b9b6fa8a6753ebec6c93a27b3c3bf434e
xinghen1995 <1097382820@qq.com>
2018-01-03 17:38:52 +0800

    rm README.md

---
+++ src/com/priv/objects/HelloDate.java
@@ -0,0 +1,10 @@
+package com.priv.objects;
+
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.print("Hello, it's: ");
+        System.out.print(new Date());
+    }
+}
