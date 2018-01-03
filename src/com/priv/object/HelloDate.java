78db5dba0af8c07d141da735a01ee136199cdf7f
xinghen1995 <1097382820@qq.com>
2018-01-03 16:50:15 +0800

    HelloDate.java

---
+++ src/com/priv/object/HelloDate.java
@@ -0,0 +1,10 @@
+package com.priv.object;
+
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.print("Hello it's: ");
+        System.out.print(new Date());
+    }
+}
